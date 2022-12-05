6e85400c78fddea309c086dd8ba2b43543795adf
sa148 <s.kurono@outlook.jp>
2022-12-05 13:32:43 +0900

    addカレンダー途中

---
+++ calendar.js
@@ -0,0 +1,5 @@
+const calendar = document.getElementById("calendar");
+const yearMonth = document.querySelector(".yearMonth");
+const days = ["月", "火", "水", "木", "金", "土", "日"];
+const date = new Data();
+let year = data.getfullyear();
